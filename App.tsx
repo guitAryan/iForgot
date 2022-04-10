eaab68eaabaa85412d7d2328936ddb42a0d3fb78
Aryan Thatte <aryan.thatte@gmail.com>
2022-04-10 19:39:50 -0400

    init yay

---
+++ App.tsx
@@ -0,0 +1,32 @@
+import { StatusBar } from 'expo-status-bar';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  )
+}
+
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+
+      <HomeScreen />
+
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
