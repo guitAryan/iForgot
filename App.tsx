8a6cc7c423bf06826074724a07df7e9327a9b35c
Aryan Thatte <aryan.thatte@gmail.com>
2022-04-11 17:50:35 -0400

    okay i'm confused but i'm getting there

--- App.tsx
+++ App.tsx
@@ -1,13 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { Button, ScrollView, StyleSheet, Text, TextInput, View, Dimensions } from 'react-native';
 
 const WINDOW_WIDTH = Dimensions.get('window').width;
 const WINDOW_HEIGHT = Dimensions.get('window').height;
 
-function Header() {
+function Header(title: string) {
   return (
     <View style={styles.header}>
-      <Text>Header Area</Text>
+      <Text>Header Area: {title}</Text>
 
     </View>
   )
@@ -16,25 +17,31 @@ function Header() {
 function Interact() {
   return (
     <View style={styles.interact}>
-      <Text>Interaction Area</Text>
-
+      <Text>Interaction Area: </Text>
     </View>
   )
 }
 
 function NavBar() {
+
   return (
     <View style={styles.navbar}>
-      <Text>Nav bar</Text>
+      <Button
+        title='Home'
+        onPress={() => Header('Home')}
+
+      />
     </View>
   )
 }
 
 export default function App() {
+  const [currentScreen, setCurrentScreen] = useState("Home")
+
   return (
     <View style={styles.default}>
       <StatusBar translucent={false} style="light" />
-      <Header />
+      <Header {...currentScreen} />
       <Interact />
       <NavBar />
     </View>
@@ -44,27 +51,26 @@ export default function App() {
 const styles = StyleSheet.create({
   default: {
     flex: 1,
-    backgroundColor: '#ffdbc2',
+    backgroundColor: '#fff5e3',
     alignItems: 'center',
     justifyContent: 'flex-start',
   },
   header: {
     alignItems: 'center',
-    borderWidth: 1,
+    backgroundColor: '#cceaff',
     height: WINDOW_HEIGHT * 0.1,
     width: WINDOW_WIDTH,
   },
   interact: {
     alignItems: 'center',
-    borderWidth: 1,
     height: WINDOW_HEIGHT * 0.8,
     width: WINDOW_WIDTH,
   },
   navbar: {
     alignItems: 'center',
-    borderWidth: 1,
+    backgroundColor: '#cceaff',
+    // position: 'absolute'
     height: WINDOW_HEIGHT * 0.1,
     width: WINDOW_WIDTH,
   },
-
 });
